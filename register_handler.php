92d5c87734fb57b73de5eeed527c629e7a170c61
Kyle Worley <kyleworley@u.boisestate.edu>
2021-03-28 21:27:01 -0600

    Attempt #7 login echoes

--- register_handler.php
+++ register_handler.php
@@ -11,6 +11,7 @@
     }
     require_once 'Dao.php';
     $dao = new Dao();
+    echo "1";
     $users = $dao->getUsers();
     $username_exists = false;
     foreach($users as $user) {
@@ -18,28 +19,30 @@
             $username_exists = true;
         }
     }
+    echo "2";
     if($username_exists) {
         $errors[] = "Invalid username: username is already in use.";
     }
-
+    echo "3";
     if(strlen($password) < 8) {
         $errors[] = "Invalid password: must be 8 or more characters.";
     }
+    echo "4";
     if(strlen($password) > 32) {
         $errors[] = "Invalid password: must be 32 or fewer characters.";
     }
-
+    echo "5";
     if(strcmp($password, $retype_password) != 0) {
         $errors[] = "Passwords do not match.";
     }
-
+    echo "6";
     if(count($errors) > 0) {
         $_SESSION['errors'] = $errors;
         echo "Errors exist";
         header('Location: register.php');
         exit;
     }
-
+    echo "7";
     $dao->addUser($username, $password);
     $_SESSION['user'] = $username;
     echo "No errors";
